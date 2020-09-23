803abbc858b82665c04d48a43b828125138b34d4
Matt Berg <matt.berg95@gmail.com>
2020-09-22 19:32:11 -0700

    Add base layout with theme

--- src/App.js
+++ src/App.js
@@ -1,33 +1,65 @@
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider,
+} from '@material-ui/core'
+import { Add as AddIcon, Home as HomeIcon } from '@material-ui/icons'
 import React from 'react'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 import './App.css'
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
 import Catch from './pages/Catch/Catch'
+import Home from './pages/Home/Home'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#c93a26',
+    },
+    secondary: {
+      main: '#d6b845',
+    },
+  },
+})
+
+const useStyles = makeStyles({
+  spacer: {
+    flexGrow: 1,
+  },
+})
 
 function App() {
+  const classes = useStyles()
   return (
-    <div className="App">
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/catch">Catch</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/catch">
-            <Catch />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <AppBar position="static">
+            <Toolbar>
+              <Link to="/">
+                <HomeIcon color="secondary"></HomeIcon>
+              </Link>
+              <div className={classes.spacer}></div>
+              <Link to="/catch">
+                <AddIcon color="secondary"></AddIcon>
+              </Link>
+            </Toolbar>
+          </AppBar>
+          <Container>
+            <Switch>
+              <Route path="/catch">
+                <Catch />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Container>
+        </Router>
+      </div>
+    </ThemeProvider>
   )
 }
 
